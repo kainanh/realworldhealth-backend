b7b09579151628778a8696ba6f89d1d407eb2da6
kainanh <kainanh@hotmail.co.uk>
2022-07-08 12:23:36 +0100

    completed endoints

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ const client = new Client(connectionString);
 client.connect();
 
 const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "https://nutribud-frontend.sigmalabs.co.uk",
-  ],
+  origin: ["http://localhost:3000"],
   credentials: true,
 };
 
@@ -31,28 +28,42 @@ app.get("/", (req, res) => {
 app.post("/login", handleLogin);
 app.delete("/login", handleUserLogout);
 app.post("/register", handleRegistration);
+app.post("/verify-session", verifySession);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 module.exports = app;
 
-async function handleLogin(req, res) {
-  const { username, password } = req.body;
-  const authorisationInfo = await loginAuthentication(username, password);
-  if (authorisationInfo.isValid) {
-    const userId = authorisationInfo.user.rows[0].id;
-    const sessionId = await createSessionId(userId);
-    res.cookie("sessionId", sessionId);
-    return res.json({ response: "Login Success!" });
-  }
-  return res
-    .status(400)
-    .json({ error: "Login failed, check details and try again." });
+async function handleLogin(server) {
+  const { email, password } = await server.body;
+  const users = (await client.queryObject(`SELECT * FROM users;`)).rows;
+  let user;
+  users.forEach((currentUser) => {
+    if (currentUser.email === email) {
+      user = currentUser;
+    }
+  });
+  const userExists = user !== undefined;
+  const passwordIsValid = userExists
+    ? await bcrypt.compare(password, user.hashed_password)
+    : false;
+  if (!(userExists && passwordIsValid))
+    return server.json({ error: "Email or password is incorrect" }, 400);
+  // EDGE CASE: user left site and deleted their cookies
+  await client.queryObject("DELETE FROM sessions WHERE user_id = $1;", user.id);
+  const sessionUUID = v4.generate();
+  await client.queryObject(
+    "INSERT INTO sessions (uuid, user_id, created_at) VALUES ($1, $2, NOW());",
+    sessionUUID,
+    user.id
+  );
+  //Cookies must be set from frontend because of netlify
+  return server.json({ response: sessionUUID });
 }
 
-async function handleUserLogout(req, res) {
-  const sessionId = req.cookies.sessionId;
+async function handleUserLogout(server, res) {
+  const sessionId = server.queryParams;
   const user = await getCurrentUser(sessionId);
   if (user.length > 0) {
     const query = `DELETE FROM sessions WHERE user_id = $1`;
@@ -62,68 +73,22 @@ async function handleUserLogout(req, res) {
   return res.status(400).json({ error: "User not logged in" });
 }
 
-async function handleRegistration(req, res) {
-  const { username, password, passwordConfirmation } = req.body;
-  const validateCredentials = await validateRegistrationCredentials(
-    username,
-    password,
-    passwordConfirmation
-  );
-  if (validateCredentials) {
-    const salt = await bcrypt.genSalt(8);
-    const hashedPassword = await hashPassword(password, salt);
-    const query =
-      "INSERT INTO users (username, hashed_password, salt) VALUES ( $1, $2, $3)";
-    await client.query(query, [username, hashedPassword, salt]);
-    const newUserId = await getNewUserId();
-    await handleGoalAddition(newUserId);
-    return res.json({ response: "Successful registration" });
+async function handleRegistration(server) {
+  const { email, password } = await server.body;
+
+  const salt = await bcrypt.genSalt(8);
+  const hashed_password = await bcrypt.hash(password, salt);
+  try {
+    await client.queryObject(
+      "INSERT INTO users (email, hashed_password, salt) VALUES ($1,$2)",
+      email,
+      hashed_password,
+      salt
+    );
+  } catch (e) {
+    return server.json({ error: "could not add user to database" }, 500);
   }
-  return res.status(400).json({ error: "Invalid credentials" });
-}
-
-async function loginAuthentication(username, password) {
-  const query = "SELECT * FROM users WHERE username = $1";
-  const existingUserCheck = await client.query(query, [username]);
-  if (existingUserCheck.rowCount > 0) {
-    const userSalt = existingUserCheck.rows[0].salt;
-    const userHashedPassword = existingUserCheck.rows[0].hashed_password;
-    const passwordEncrypted = await hashPassword(password, userSalt);
-    if (passwordEncrypted === userHashedPassword) {
-      return { isValid: true, user: existingUserCheck };
-    }
-  }
-  return { isValid: false };
-}
-
-async function validateRegistrationCredentials(
-  username,
-  password,
-  passwordConformation
-) {
-  const query = "SELECT * FROM users WHERE username = $1";
-  const duplicateUsernameCheck = await client.query(query, [username]);
-  if (
-    duplicateUsernameCheck.rowCount < 1 &&
-    password === passwordConformation &&
-    password.length > 1
-  ) {
-    return true;
-  }
-  return false;
-}
-
-async function hashPassword(password, salt) {
-  const hashedPassword = await hasher.hash(password, salt);
-  return hashedPassword;
-}
-
-async function createSessionId(userId) {
-  const sessionId = crypto.randomUUID();
-  const query =
-    "INSERT INTO sessions (uuid, user_id, created_at) VALUES ($1, $2, NOW())";
-  await client.query(query, [sessionId, userId]);
-  return sessionId;
+  server.json({ response: "User added successfully" }, 200);
 }
 
 async function getCurrentUser(sessionId) {
@@ -131,4 +96,16 @@ async function getCurrentUser(sessionId) {
     "SELECT * FROM users JOIN sessions ON users.id = sessions.user_id WHERE sessions.created_at < NOW() + INTERVAL '7 DAYS' AND sessions.uuid = $1";
   const user = await client.query(query, [sessionId]);
   return user.rows;
+}
+
+async function verifySession(server) {
+  const { sessionID } = server.queryParams;
+  const sessions = (await client.queryObject("SELECT * FROM sessions")).rows;
+
+  let isValid = false;
+  sessions.forEach((session) => {
+    if (session.uuid === sessionID) isValid = true;
+  });
+
+  return server.json({ response: isValid });
 }
